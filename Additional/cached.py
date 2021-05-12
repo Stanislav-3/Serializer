f985ed7339d463fe44883d762490f8e0196ebde1
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2021-05-12 20:26:22 +0300

    Fixed bugs

--- Additional/cached.py
+++ Additional/cached.py
@@ -1,19 +1,46 @@
+from inspect import getfullargspec
+
+
 def cached(func):
     """
     Декоратор @cached, который сохраняет значение функции при каждом вызове.
     Если функция вызвана повторно с теми же аргументами, то возвращается сохраненное значение,
-    а функция не вычисляется. Учесть как позиционные, так и именованные аргументы.
+    а функция не вычисляется.
     """
     cache = []
     results = {}
-    def resulting_func(*args, **kwargs):
 
-        if (args, kwargs) in cache:
-            print('Cached: ', results[cache.index((args, kwargs))])
+    def resulting_func(*args, **kwargs):
+        full_arg_spec = getfullargspec(func)
+        args_name = full_arg_spec[0]
+        arguments = {}
+
+        kwargs_ = kwargs.copy()
+
+        args_len = len(args)
+        for i in range(min(len(args_name), args_len)):
+            arguments[args_name[i]] = args[i]
+
+        while len(args_name) - args_len > 0:
+            arguments[args_name[args_len]] = kwargs[args_name[args_len]]
+            del kwargs_[args_name[args_len]]
+            args_len += 1
+
+        if full_arg_spec[1] is not None:
+            arguments['*args'] = args[len(args_name):]
+
+        if full_arg_spec[2] is not None:
+            arguments['**kwargs'] = kwargs_
+
+        print(arguments)
+        if arguments in cache:
+            print('Cached:', end=' ')
+            res = results[cache.index(arguments)]
         else:
             print('Not cached:', end=' ')
-            cache.append((args, kwargs))
             res = func(*args, **kwargs)
+            cache.append(arguments)
             results[len(cache) - 1] = res
+        return res
 
     return resulting_func
--- Additional/main.py
+++ Additional/main.py
@@ -1,20 +1,19 @@
 from cached import cached
 
+
 @cached
-def f(*args, **kwargs):
-    values = [*args, *list(kwargs.values())]
+def f(x, y, *args, **kwargs):
+    sum_ = x + y
+    sum_ += sum(args)
+    sum_ += sum(kwargs.values())
 
-    print('Sum( ', end='')
-    sum = 0
-    for i in range(len(values)):
-        print(values[i], end=' ')
-        sum += values[i]
-    print(') = ', sum)
-
-    return sum
+    return sum_
 
 if __name__ == '__main__':
-    f(1, 2, f=3)
-    f(1, 2, f=4)
-    f(1, 2, f=3)
+    print(f(1, 2))
+    print(f(1, y=2))
+    print(f(x=1, y=2))
 
+    print(f(1, 2, 3))
+    print(f(1, 2, other=3))
+    print(f(1, 2, 3, other=4))
